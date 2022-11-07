1ef68ec91aa7e11f78d01c955b2842cb0aba805e
riyoneri <lionkaneza@gmail.com>
2022-11-07 07:26:33 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Fragment, useState } from 'react';
+
+import Header from './component/Layout/Header'
+import Meals from './component/Meals/Meals';
+import Cart from './component/Cart/Cart';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false)
+
+  const showCartHandler = () => {
+    setCartIsShown(true)
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false)
+  }
+
+  return (
+    <Fragment>
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals /> 
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
